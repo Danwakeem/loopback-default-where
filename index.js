ae4195277a0b5c57fb9f98dfeb0868e418c20b3f
Danwakeem <danwakeem@gmail.com>
2019-05-28 18:34:16 -0500

    Linted

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const util = require("util");
 const defaultFilter = require("./src/index");
 
 module.exports = util.deprecate(
-  app => app.loopback.modelBuilder.mixins.define("DefaultFilter", defaultFilter),
+  app =>
+    app.loopback.modelBuilder.mixins.define("DefaultFilter", defaultFilter),
   "DEPRECATED: Use mixinSources, see https://github.com/clarkbw/loopback-ds-timestamp-mixin#mixinsources"
 );
