66d19304c9365c24bb2f7515f15f221fff678689
Danwakeem <danwakeem@gmail.com>
2019-05-28 22:13:11 -0500

    Updated package

--- index.js
+++
@@ -1,8 +0,0 @@
-const util = require("util");
-const defaultFilter = require("./src/index");
-
-module.exports = util.deprecate(
-  app =>
-    app.loopback.modelBuilder.mixins.define("DefaultFilter", defaultFilter),
-  "DEPRECATED: Use mixinSources, see https://github.com/clarkbw/loopback-ds-timestamp-mixin#mixinsources"
-);
